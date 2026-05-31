634ec75bddd01d5b8a6239779b45d2276386a030
Alexandra Suslova <alexandra11622@yandex.ru>
2026-05-31 13:32:54 +0300

    сделала усечение строки

---
+++ 1-module/5-task/index.js
@@ -0,0 +1,8 @@
+function truncate(str, maxlength) {
+  return (str.length > maxlength)
+    ? str.slice(0, maxlength - 1) + '…'
+    : str;
+}
+
+console.log(truncate('Вот, что мне хотелось бы сказать на эту тему:', 20) === 'Вот, что мне хотело…');
+console.log(truncate('Всем привет!', 20) === 'Всем привет!');
